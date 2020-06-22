6463e85a7ec45a2e20d3275e4736f3cd89dc08f8
avkarra <avkarra@ncompasbusiness.com>
2020-06-22 09:21:23 +0530

    SnapShot testing added for dialog-table.test.js

--- src/components/DialogTable/dialog-table.test.js
+++ src/components/DialogTable/dialog-table.test.js
@@ -15,4 +15,45 @@ it('should render without exploding', () => {
     ];
     ReactDOM.render(
         <DialogTable data={data}></DialogTable>, div)
+});
+
+
+it('matches snapshot', () => {
+    const data = [
+        {
+            "academyId": 30015,
+            "serialNumber": "TT-C67ML-A-0001-02522",
+            "avgDailyDischarge": 1.0580
+        }, {
+            "academyId": 30015,
+            "serialNumber": "1805C67HD02212",
+            "avgDailyDischarge": 0.4624
+        }, {
+            "academyId": 30015,
+            "serialNumber": "NGTT-0040",
+            "avgDailyDischarge": 0.3634
+        }, {
+            "academyId": 30015,
+            "serialNumber": "1805C67HD02244",
+            "avgDailyDischarge": 0.3293
+        }, {
+            "academyId": 30015,
+            "serialNumber": "1805C67HD02440",
+            "avgDailyDischarge": 0.318
+        }, {
+            "academyId": 30015,
+            "serialNumber": "TT-C67ML-A-0001-02573",
+            "avgDailyDischarge": 0.3094
+        }, {
+            "academyId": 30015,
+            "serialNumber": "TT-C67ML-A-0001-01355",
+            "avgDailyDischarge": 0.2856
+        }, {
+            "academyId": 30015,
+            "serialNumber": "1805C67HD01980",
+            "avgDailyDischarge": 0.2129
+        }
+    ];
+    const tree = ReactTestRenderer.create(<DialogTable data={data}></DialogTable>).toJSON();
+    expect(tree).toMatchSnapshot();
 })
